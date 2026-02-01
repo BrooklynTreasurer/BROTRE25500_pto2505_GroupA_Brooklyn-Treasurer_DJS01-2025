b58e8da0b662173e1e9310bbe4567355f85f5c29
Brooklyn <markmclean2005@gmail.com>
2026-02-01 10:39:38 +0200

    updated create podcastCard to formate dates

--- src/components/createPodcastCard.js
+++ src/components/createPodcastCard.js
@@ -1,24 +1,26 @@
+import { podcasts } from '../data.js';
+import * as dates from '../utils/DateUtils.js';
+
 /**
  * createPodcastCard - Creates a podcast card element.
  *
- * @param {Object} podcast - The podcast data.
+ * @param {Object} podcastss - The podcast data.
  * @param {string} podcast.title - The title of the podcast.
  * @param {string} podcast.description - The description of the podcast.
  * @param {string} podcast.image - The URL of the podcast image.
  * @return {HTMLElement} The podcast card element.
  */
 
-export function createPodcastCard(podcast) {
+export function createPodcastCard(podcasts) {
     const card = document.createElement('div');
     card.className = 'podcast-card';
     card.innerHTML = `
-        <img src="${podcast.image}" alt="${podcast.title} Cover" class="podcast-image"/>
+        <img src="${podcasts.image}" alt="${podcasts.title} Cover" class="podcast-image"/>
          <div class="podcast-info">
-            <h3 class="podcast-title">${podcast.title}</h3>
-            <p class="podcast-description">${podcast.description}</p>
-            <p class="podcast-seasons">${podcast.seasons} Seasons</p>
-            <p class="genres">${podcast.genres.join(', ')}</p>
-            <p class="podcast-update">Last Updated: ${new Date(podcast.lastUpdated).toLocaleDateString()}</p>
+            <h3 class="podcast-title">${podcasts.title}</h3>
+            <p class="podcast-seasons">${podcasts.seasons} Seasons</p>
+            <p class="genres">${podcasts.genres.join(', ')}</p>
+            <p class="podcast-update">Last Updated: ${dates.formatDate(podcasts.updated).toLocaleDateString()}</p>
      </div>
     `;
     return card;
